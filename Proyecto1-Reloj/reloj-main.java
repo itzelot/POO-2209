96dc8f2a272cdf80f4be9058b25a2fc09d5c887d
itzelot <itzelot01@gmail.com>
2020-02-17 20:35:13 -0600

    Proyecto java Reloj

---
+++ Proyecto1-Reloj/reloj-main.java
@@ -0,0 +1,17 @@
+package ProyectoReloj;
+
+import ico.fes.poo.Reloj; 
+
+public class Proyecto2 {
+
+    
+
+    public static void main(String[] args) {       
+        
+        Reloj prueba = new Reloj(23,59,1);
+        prueba.mostrarhora(); // Llamamos al metodo mostrarHora()
+                
+    }
+    
+}
+
